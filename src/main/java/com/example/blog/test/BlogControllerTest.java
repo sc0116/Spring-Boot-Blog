69ea1ffdca7b3b0229e0b9c1c86f704f1466f9b2
SeungCheol <ssc6839@gmail.com>
2021-07-22 03:00:24 +0900

    환경설정

---
+++ src/main/java/com/example/blog/test/BlogControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+    @GetMapping("/test/hello")
+    public String hello() {
+        System.out.println("hello spring boot");
+        return "<h1>hello spring boot</h1>";
+    }
+}
