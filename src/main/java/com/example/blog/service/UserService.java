446d619001a48b1b30d7c0c19381a5fde4b09dd6
SeungCheol <ssc6839@gmail.com>
2021-07-31 17:04:25 +0900

    기본 로그인

--- src/main/java/com/example/blog/service/UserService.java
+++ src/main/java/com/example/blog/service/UserService.java
@@ -4,8 +4,7 @@ import com.example.blog.model.User;
 import com.example.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service    //스프링이 컴포턴트 스캔을 통해서 Bean에 등록해줌(IoC를 해줌)
 public class UserService {
@@ -16,5 +15,10 @@ public class UserService {
     @Transactional
     public void 회원가입(User user) {
         userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true) //Select할 때 트랜잭션 시작, 서비스 종료시에 트랜잭션 종료(정합성)
+    public User 로그인(User user) {
+        return userRepository.findByUsernameAndPassword(user.getUsername(), user.getPassword());
     }
 }
--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,6 +3,9 @@ let index = {
         $("#btn-save").on("click", () => {  //this를 바인딩하기 위해서 () => {}사용, function() X
             this.save();
         });
+        $("#btn-login").on("click", () => {
+           this.login();
+        });
     },
     save: function () {
         let data = {
@@ -22,6 +25,25 @@ let index = {
             dataType: "json"    //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => js 오브젝트로 변경해줌
         }).done(function (res) {
             alert("회원가입이 완료되었습니다.");
+            location.href="/blog";
+        }).fail(function (err) {
+            alert(JSON.stringify(err));
+        });
+    },
+    login: function () {
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val()
+        };
+
+        $.ajax({
+            type: "post",
+            url: "/blog/api/user/login",
+            data: JSON.stringify(data),  //http body데이터
+            contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
+            dataType: "json"    //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => js 오브젝트로 변경해줌
+        }).done(function (res) {
+            alert("로그인이 완료되었습니다.");
             location.href="/blog";
         }).fail(function (err) {
             alert(JSON.stringify(err));
