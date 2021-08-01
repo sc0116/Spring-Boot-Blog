e59c4df5ed585e14866a511a736d0253270577e6
SeungCheol <ssc6839@gmail.com>
2021-08-01 14:32:21 +0900

    시큐리티 로그인

--- src/main/java/com/example/blog/controller/api/UserApiController.java
+++ src/main/java/com/example/blog/controller/api/UserApiController.java
@@ -18,23 +18,11 @@ public class UserApiController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/api/user")
+    @PostMapping("/auth/joinProc")
     public ResponseDto<Integer> save(@RequestBody User user) {
         System.out.println("UserApiController: save 호출됨");
         user.setRole(RoleType.USER);
         userService.회원가입(user);
-
-        return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
-    }
-
-    @PostMapping("/api/user/login")
-    public ResponseDto<Integer> login(@RequestBody User user, HttpSession session) {
-        System.out.println("UserApplication: login 호출됨");
-        User principal = userService.로그인(user);  //principal(접근주체)
-
-        if(principal != null) {
-            session.setAttribute("principal", principal);
-        }
 
         return new ResponseDto<Integer>(HttpStatus.OK.value(), 1);
     }
--- src/main/java/com/example/blog/repository/UserRepository.java
+++ src/main/java/com/example/blog/repository/UserRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 //@Repository 어노테이션 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    //JPA Naming 전략
-    //SELECT * FROM user WHERE username - ? AND password = ?;
-    User findByUsernameAndPassword(String username, String password);
-
-    //위에 한줄과 아래 두줄이 동일
-    //@Query(value="SELECT * FROM user WHERE username = ? AND password = ?", nativeQuery=true)
-    //User login(String username, String password);
 }
+
+//JPA Naming 전략
+//SELECT * FROM user WHERE username - ? AND password = ?;
+//User findByUsernameAndPassword(String username, String password);
+
+//위에 한줄과 아래 두줄이 동일
+//@Query(value="SELECT * FROM user WHERE username = ? AND password = ?", nativeQuery=true)
+//User login(String username, String password);
