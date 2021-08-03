094706a9addb67c4a423406e21e5e7069fac8e36
SeungCheol <ssc6839@gmail.com>
2021-08-03 23:03:55 +0900

    글목록 페이징하기

--- src/main/java/com/example/blog/controller/BoardController.java
+++ src/main/java/com/example/blog/controller/BoardController.java
@@ -3,6 +3,9 @@ package com.example.blog.controller;
 import com.example.blog.config.auth.PrincipalDetail;
 import com.example.blog.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,9 +20,9 @@ public class BoardController {
     //컨트롤러에서 세션을 어떻게 찾는지?
     //@AuthenticationPrincipal PrincipalDetail principal
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @PageableDefault(size = 3, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
 
-        model.addAttribute("boards", boardService.글목록());
+        model.addAttribute("boards", boardService.글목록(pageable));
         //resources/templates/index.html
         return "index"; //viewResolver 작동
     }
--- src/main/java/com/example/blog/service/BoardService.java
+++ src/main/java/com/example/blog/service/BoardService.java
@@ -4,10 +4,10 @@ import com.example.blog.model.Board;
 import com.example.blog.model.User;
 import com.example.blog.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class BoardService {
@@ -22,7 +22,9 @@ public class BoardService {
         boardRepository.save(board);
     }
 
-    public List<Board> 글목록() {
-        return boardRepository.findAll();
+    public Page<Board> 글목록(Pageable pageable) {
+        return boardRepository.findAll(pageable);
     }
+
+
 }
