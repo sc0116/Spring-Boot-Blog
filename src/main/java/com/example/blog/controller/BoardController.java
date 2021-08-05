5ce62e4468abd12cbeb0a9bd2d24431955db507b
SeungCheol <ssc6839@gmail.com>
2021-08-05 14:27:51 +0900

    글수정하기

--- src/main/java/com/example/blog/controller/BoardController.java
+++ src/main/java/com/example/blog/controller/BoardController.java
@@ -35,6 +35,13 @@ public class BoardController {
         return "board/detailForm";
     }
 
+    @GetMapping("/board/{id}/updateForm")
+    public String updateForm(@PathVariable int id, Model model) {
+
+        model.addAttribute("board", boardService.글상세보기(id));
+        return "board/updateForm";
+    }
+
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "board/saveForm";
--- src/main/resources/static/js/board.js
+++ src/main/resources/static/js/board.js
@@ -5,7 +5,10 @@ let index = {
         });
         $("#btn-delete").on("click", () => {
             this.deleteById();
-        })
+        });
+        $("#btn-update").on("click", () => {
+            this.update();
+        });
     },
     save: function () {
         let data = {
@@ -27,7 +30,7 @@ let index = {
         });
     },
     deleteById: function () {
-        var id = $("#id").text();
+        let id = $("#id").text();
 
         $.ajax({
             type: "delete",
@@ -35,6 +38,26 @@ let index = {
             dataType: "json"    //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => js 오브젝트로 변경해줌
         }).done(function (res) {
             alert("삭제가 완료되었습니다.");
+            location.href="/";
+        }).fail(function (err) {
+            alert(JSON.stringify(err));
+        });
+    },
+    update: function () {
+        let id = $("#id").val()
+        let data = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        };
+
+        $.ajax({
+            type: "put",
+            url: "/api/board/" + id,
+            data: JSON.stringify(data),  //http body데이터
+            contentType: "application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
+            dataType: "json"    //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열(생긴게 json이라면) => js 오브젝트로 변경해줌
+        }).done(function (res) {
+            alert("글수정이 완료되었습니다.");
             location.href="/";
         }).fail(function (err) {
             alert(JSON.stringify(err));
